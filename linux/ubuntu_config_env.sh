1666664be5da3c5dfd550ba0939c0bcae2f8a4f7
polotto <angelopolotto@gmail.com>
2019-10-28 18:09:10 -0300

    ubuntu configs added

---
+++ linux/ubuntu_config_env.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+##########################
+# script para instalar os softwares necessários
+# para desenvolvimento para Ubuntu
+##########################
+
+# exige o super usuário
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
+# configura proxy
+chmod +x ./ubuntu_set_proxy.sh
+./ubuntu_set_proxy.sh
+
+# update apt
+apt -y update
+
+# upgrade apt
+apt -y upgrade
+
+# git
+apt -y install git
+
+# docker
+apt -y install docker.io
+
+# python pip3
+apt -y install python3-pip
+
+# postman
+snap install postman
+
+# intellij ultimate
+snap install --classic intellij-idea-ultimate
+
+# vs code
+snap install --classic code
+
+# android studio
+snap install --classic android-studio
+
+# github desktop
+snap install --beta --classic github-desktop
